164f4f5da931d8e9e27e9cb18a14e80302a01a92
Thimo <Thimo@it4me.nl>
2022-12-21 14:10:09 +0100

    World done (kinda)

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -57,7 +57,7 @@ public class PlayerMovement : MonoBehaviour
 
         if (IsGrounded())
         {
-            rb.drag = 1;
+            rb.drag = 3;
             rb.AddForce(0.2f * moveSpeed / Time.deltaTime * moveDirection.normalized, ForceMode.Force);
         }
             
