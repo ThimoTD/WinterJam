5107a3b507ba4e3fc611c63e623559bc488e8585
Thimo <Thimo@it4me.nl>
2022-12-21 17:11:02 +0100

    build begin

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public static class Inventory
 {
-    public static int snowballs = 9;
-    public static int stones = 10;
-    public static int carrot = 1;
-    public static int scarf = 1;
-    public static int sticks = 2;
-    public static int tophat = 1;
-    public static int coins = 5;
+    public static int snowballs;
+    public static int stones;
+    public static int carrot;
+    public static int scarf;
+    public static int sticks;
+    public static int tophat;
+    public static int coins;
     public static int key;
     public static int trowableSnowball;
 }
